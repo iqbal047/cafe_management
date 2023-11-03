86b98fdde1a48fda33eaa4678d62dee23a44479e
Md Iqbal Hossain <mdiqbalhossain20775@gmail.com>
2023-11-03 22:46:22 +0600

    cafe management system file added.

---
+++ com.inn.cafe/src/main/java/com/inn/cafe/serviceimpl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.inn.cafe.serviceimpl;
+
+import com.inn.cafe.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
